b1c4e89bd401e7e2fdaf16165e0dd169b6f31eef
Justin Herpin <justindherpin@yahoo.com>
2021-11-10 22:14:47 -0800

    End of Day 11/10/2021. HTML/CSS 90% MVP. Started javascript/JQuery.

--- OG/app.js
+++ OG/app.js
@@ -1,9 +1,13 @@
 $(() => {
 
+    console.log("ready")
 
+    $("#stacks").click(function(e) {
+        // e.target.style.backgroundColor = "purple";
+        $(e.target).css("background-color", "purple");
+    })
 
-
-
+    // $("#disc1").css("background-color", "purple");
 
 
 
