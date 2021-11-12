441cc7ee6391de4156711367c6d1944103e24e74
Justin Herpin <justindherpin@yahoo.com>
2021-11-12 15:56:47 -0800

    11/12/21 some psuedo code ideas/playing around with functions and starting gameplay functions.  Orgainizing JavaScript.

--- OG/app.js
+++ OG/app.js
@@ -1,41 +1,78 @@
 $(() => {
 
-//    console.log("ready")
-    
-    
+
+//-----------------  Declaring variables for global use  --------------------------
+
+    let $selectedDisc = null;
+    let startDivArray = [];
+    let stagingDivArray = [];
+    let targetDivArray = [];
+
+
+
+//------------------  functions/logic for play, rules, and story button(s) functionality  --------------------------
+
     $("#navBox").click(function(e) {
+        
         if($(e.target).attr("id") === "rules") {
             $("#rulesBox").css("display", "block");
-            //                      console.log($(e.target));
+            //                                                  $("#navBox").off("click");  attempting to 
+           
         } else if($(e.target).attr("id") === "story") {
             $("#storyBox").css("display", "block");
+
         } else if($(e.target).attr("id") === "play") {
             $(".disc").css("display", "block");
+
         };
+
+        // $("#navBox").click(function(e) {
+            //     let modalClass = 
+            //     $(".modal").css("display", "block");  $(`$(e.target).modal $(`.modal$(e.target`) 
+            // });
+            //                          let targetModal = `$(.modal$(e.target))`
+
 
     });
 
-    // $("#navBox").click(function(e) {
-    //     let modalClass = 
-    //     $(".modal").css("display", "block");
-    // });
 
-
+//-------------------  function to close pop-ups once clicked:  --------------------------
 
     $(".close").click(() => {
         $(".modal").css("display", "none")
     })
 
 
-    $("#stacks").on("click", ".disc", function(e) {
-    //                                 console.log($target);
-    //                                 let $target = $("e.target")
-    //                                 $("e.target").stopImmediatePropagation();
-    //                                 e.target.style.backgroundColor = "purple";
-         $(e.target).css("background-color", "purple");
-    });
+//-------------------  Gameplay:    ------------------------------------------------------
 
-    //                                  $("#disc1").css("background-color", "purple");
+    function play() {
+        
+
+    }
+
+
+    // $("#stacks").on("click", ".disc", function(e) {
+    // //                                 console.log($target);
+    // //                                 let $target = $("e.target")
+    // //                                 $("e.target").stopImmediatePropagation();
+    // //                                 e.target.style.backgroundColor = "purple";
+    //      $(e.target).css("background-color", "purple");
+    // });
+
+    // $("#stacks").click(function() {
+        
+        
+    // })
+
+    //     function $selectDisc(e) {
+    //                 $selectedDisc = $(e.target);
+    //                 $(e.target).css("background-color", "purple");
+    //             }
+                
+    //     function $moveDisc(e) {
+    //         $(e.target).append($selectedDisc);
+    //     }
+    // //                                  $("#disc1").css("background-color", "purple");
 
 
 
