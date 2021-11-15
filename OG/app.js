fa97e80cf198f56dac4b5bdbc9f1fd06d9d65f45
Justin Herpin <justindherpin@yahoo.com>
2021-11-15 15:17:10 -0800

    Worked through javascript block with Billie.  Continuing javascript and gameplay function.

--- OG/app.js
+++ OG/app.js
@@ -4,17 +4,18 @@ $(() => {
 //-----------------  Declaring variables for global use  --------------------------
 
      let selectedDisc = [];
+     let winningCompareArray = [1,2,3];
      let startDivArray = [1,2,3];
-    // let stagingDivArray = [];
-    // let targetDivArray = [];
+     let stagingDivArray = [];
+     let targetDivArray = [1,2,3];
     
-     function $selectDisc (e) {
-        //        console.log(e.target);
-        $(e.target).css("background-color", "purple");
-        selectedDisc.push($(e.target).data("index"));
-        console.log(selectedDisc);
-        console.log(selectedDisc[0]);
-    };
+    //  function $selectDisc (e) {
+    //     //        console.log(e.target);
+    //     $(e.target).css("background-color", "purple");
+    //     //selectedDisc.push($(e.target).data("index"));
+    //     console.log(selectedDisc);
+    //     console.log(selectedDisc[0]);
+    // };
     //                                                      function test() {
     //                                                          console.log("GREAT JOB!!  that worked!")
     //                                                      }
@@ -62,14 +63,54 @@ $(() => {
 
     
     function play() {
+        //console.log(selectedDisc);
 
-        console.log(selectedDisc);
-        if (selectedDisc[0] === undefined) {
-            console.log("it's undefined");
-        } else {
-            console.log("the loop worked")
-        }
-        console.log(selectedDisc[0]);
+        $("#stacks .discDiv").on("click", firstClick)
+
+        function firstClick(e) {
+            
+            let clickedDiv = e.target;
+            console.log(clickedDiv);
+            // console.log($(e.target).find(":first-child"));
+            // $(e.target).find(":first-child").css("background-color", "purple");
+            // console.log(clickedDiv);
+            // selectedDisc = [$(e.target.firstchild)]
+            // $(selectedDisc).css("background-color", "purple");
+            // console.log(clickedDiv);
+            // console.log(selectedDisc)
+
+            if (selectedDisc.length === 0) {
+                if(e.target.classlist.contains("discDiv")) {
+                    $discToAppend = $(e.target).find(":first-child");
+                    $discToAppend.css("background-color", "purple");
+
+                } else {
+                    $discToAppend = $(e.target)
+                    $discToAppend.css("background-color", "purple");
+                    console.log("this is e.target" + $discToAppend);
+                }
+    //                                                                  $(e.target).find(":first-child").css("background-color", "purple");
+                    selectedDisc.push($(e.target).find(":first-child").data("index"));
+                    console.log(selectedDisc);
+                //console.log("this is where it should select a disc and change it's color");
+
+            } else if(selectedDisc.length === 1) {
+                    console.log("this is where it will be appended");
+                    $discToAppend.prependTo(clickedDiv);
+                    selectedDisc = [];
+            //    console.log($("#discDiv1"));
+            //    clickedDiv.prepend($("#disc1"))
+            //     e.target.append()
+            
+            } else if  (targetDivArray.length === winningCompareArray.length){
+            //                                                  alert("You Won!!!  Great JOB!")
+            console.log(selectedDisc[0]);
+            }
+            
+
+           
+        };
+    
 
     //     if (selectedDisc[0] === undefined) {
 
