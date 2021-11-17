784cbc58d4886a0dc74a896763b65788a7ad4ec2
Justin Herpin <justindherpin@yahoo.com>
2021-11-17 14:05:56 -0800

    console.log() of win condition registering.  moving on to implementing solution with rest of code.

--- OG/app.js
+++ OG/app.js
@@ -8,7 +8,8 @@ $(() => {
      let startDivArray = [1,2,3];
      let stagingDivArray = [];
      let targetDivArray = [];
-    
+    //console.log(targetDivArray.length);
+    //checkForWin(targetDivArray, winningCompareArray);
 
 //------------------  functions/logic for nav items: play, rules, and story button(s) functionality  --------------------------
 
@@ -49,65 +50,50 @@ $(() => {
         $("#stacks .discDiv").on("click", firstClick)
         
         function firstClick(e) {
-            console.log(e.target);
-            console.log(e.delegateTarget);
             let $clickedDiv = e.delegateTarget;
             // console.log($clickedDiv);
             // console.log(selectedDisc)
+            //checkForWin(targetDivArray, winningCompareArray);
 
 
     //---------------------  Conditionals to select and move discs:  --------------------------
 
-
+            //noWinYet:
             if (selectedDisc.length === 0) {
-                //if($(e.target).classlist.contains(discDiv)) {
-                    $discToAppend = $(e.delegateTarget).find(":first-child");
-                    $discToAppend.addClass("selected");
+                $discToAppend = $(e.delegateTarget).find(":first-child");
+                $discToAppend.addClass("selected");
+                selectedDisc.push($(e.delegateTarget).find(":first-child").data("index"));
+                console.log("after step one: " + selectedDisc);
 
-                //} else {
-                    //$discToAppend = $(e.target)
-                    //$discToAppend.css("background-color", "purple");
-                    //$discToAppend.addClass("selected");
-                    //console.log("this is e.target" + $discToAppend);
-                //}
- 
-                    selectedDisc.push($(e.delegateTarget).find(":first-child").data("index"));
-                    console.log("after step one: " + selectedDisc);
 
 //----------------  Set Conditionals to remove selected disc from current array for tracking of moves  ----------  
 
-                    if ($($clickedDiv).attr("id") === "discDiv1") {
-                        startDivArray.shift()
-                        console.log("this is where you remove selected disc from discDiv1's array: " + startDivArray);
+                if ($($clickedDiv).attr("id") === "discDiv1") {
+                    startDivArray.shift()
+                    console.log("this is where you remove selected disc from discDiv1's array: " + startDivArray);
 
-                    } else if ($($clickedDiv).attr("id") === "discDiv2") {
-                        stagingDivArray.shift()
-                        console.log("this is where you remove selected disc from discDiv2's array: " + stagingDivArray);
+                } else if ($($clickedDiv).attr("id") === "discDiv2") {
+                    stagingDivArray.shift()
+                    console.log("this is where you remove selected disc from discDiv2's array: " + stagingDivArray);
 
-                    } else if ($($clickedDiv).attr("id") === "discDiv3") {
-                        targetDivArray.shift()
-                        console.log("this is where you remove selected disc from disDiv3's array: " + targetDivArray);
+                } else if ($($clickedDiv).attr("id") === "discDiv3") {
+                    targetDivArray.shift()
+                    console.log("this is where you remove selected disc from disDiv3's array: " + targetDivArray);
 
-                    }
+                }
                 
 //----------------  Set Conditionals after disc is selected to control moving selected disc (seletedDisc.lenth === 1) ----------
 
             } else if (selectedDisc.length === 1) {
-                console.log($($clickedDiv).attr("id"));
-                console.log($($clickedDiv).data("array"));
-               
-//                    selectedDisc = [];
+                //checkForWin(targetDivArray, winningCompareArray);  /////move this if doesn't work
                     if ($($clickedDiv).attr("id") === "discDiv1") {
-                        console.log("you made it to right before your array comparisons")
-                        console.log(targetDivArray[0]);
-                        console.log(selectedDisc[0]);
 
                         if (startDivArray[0] > selectedDisc[0] || startDivArray[0] == undefined) {
                             console.log($discToAppend);
                             $discToAppend.removeClass("selected");
                             $discToAppend.prependTo($clickedDiv);
                             startDivArray.push($($discToAppend).data("index"))
-                            console.log("this is where you update discDiv1's array: " + startDivArray);
+                            console.log("discDiv1's array => " + startDivArray);
 
                         } else {
                             $discToAppend.removeClass("selected");
@@ -115,26 +101,24 @@ $(() => {
                         }
                         
                     } else if ($($clickedDiv).attr("id") === "discDiv2") {
+
                         if (stagingDivArray[0] > selectedDisc[0] || stagingDivArray[0] == undefined) {
                             $discToAppend.removeClass("selected");
                             $discToAppend.prependTo($clickedDiv);
                             stagingDivArray.push($($discToAppend).data("index"))
-                            console.log("this is where you update discDiv2's array: " + stagingDivArray);
+                            console.log("discDiv2's array => " + stagingDivArray);
                         } else {
                             $discToAppend.removeClass("selected");
                             selectedDisc = [];
                         }
 
                     } else if ($($clickedDiv).attr("id") === "discDiv3") {
-                        console.log("you made it");
-                        console.log(targetDivArray[0]);
-                        console.log(selectedDisc[0]);
-                        if (targetDivArray[0] > selectedDisc[0] || targetDivArray[0] == undefined) {
 
+                        if (targetDivArray[0] > selectedDisc[0] || targetDivArray[0] == undefined) {
                             $discToAppend.removeClass("selected");
                             $discToAppend.prependTo($clickedDiv);
                             targetDivArray.push($($discToAppend).data("index"))
-                            console.log("this is where you update disDiv3's array: " + targetDivArray);
+                            console.log("disDiv3's array => " + targetDivArray);
 
                         } else {
                             $discToAppend.removeClass("selected");
@@ -142,7 +126,7 @@ $(() => {
                         }
 
                     } 
-                
+                checkForWin(targetDivArray, winningCompareArray);  /////move this if doesn't work
                 // } else { function illegalMoveAlert() {
                 //     preventDefault()
                 //     alert("You cannot put a larger disc on top of a smaller disc.  Please try again!");
@@ -163,6 +147,62 @@ $(() => {
 
 
     };
+
+    // function checkForWin() {
+    //     console.log("check for win invoked");
+    //     if (targetDivArray.length === winningCompareArray.length) {
+    //         console.log("targetdivarray does === winningcomparearray");
+    //         if (targetDivArray.join() === winningCompareArray.join()) {
+    //             console.log("you win!!!");
+
+    //         } else {
+    //             console.log("keep trying!")
+    //         }
+    //     } else {
+    //         console.log("Keep trying");
+    //     }
+    // }
+
+    // function checkForWin(a, b) {
+    //     console.log(a.length);
+    //     for (let i = 0; i < a.length; i++) {
+    //         if (targetDivArray[i] === winningCompareArray[i]) {
+    //             console.log("win condition achieved");
+
+    //         } else {
+    //             console.log("must keep going...win condition false");
+    //         }
+    //     }
+
+    // }
+    function checkForWin(a, b) {
+        console.log(a);
+        let targ = a.sort();
+        let win = b.sort();
+        console.log(targ);
+        console.log(a.length);
+        if (targ.length === win.length) {
+            for (let i = 0; i < b.length; i++) {
+                if (a[i] === b[i]) {
+                    console.log("win condition achieved");
+
+                } else {
+                    console.log("must keep going...win condition false");
+                }
+            }
+        } else {
+            console.log("keep going.  array lengths not equal");
+        }
+    }
+    // function checkForWin(a,b) {
+    //     a.toString();
+    //     b.toString();
+    //     if (a === b) {
+    //         console.log("win solution executed here");
+    //     } else {
+    //         console.log("Keep going....no win yet");
+    //     }
+    // }
 
     // function checkIfLegal() {
 
