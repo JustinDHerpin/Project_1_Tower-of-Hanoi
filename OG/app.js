8f35939d64cb3c55c76b27f36ce2f16f2a0277f9
Justin Herpin <justindherpin@yahoo.com>
2021-11-16 18:20:10 -0800

    Class over.  Fixed issue for conditionals not stopping illegal moves also not working for valid moves.

--- OG/app.js
+++ OG/app.js
@@ -7,19 +7,8 @@ $(() => {
      let winningCompareArray = [1,2,3];
      let startDivArray = [1,2,3];
      let stagingDivArray = [];
-     let targetDivArray = [1,2,3];
+     let targetDivArray = [];
     
-    //  function $selectDisc (e) {
-    //     //        console.log(e.target);
-    //     $(e.target).css("background-color", "purple");
-    //     //selectedDisc.push($(e.target).data("index"));
-    //     console.log(selectedDisc);
-    //     console.log(selectedDisc[0]);
-    // };
-    //                                                      function test() {
-    //                                                          console.log("GREAT JOB!!  that worked!")
-    //                                                      }
-
 
 //------------------  functions/logic for nav items: play, rules, and story button(s) functionality  --------------------------
 
@@ -27,31 +16,24 @@ $(() => {
       
     function handleNavClick (e) {
         
-        if($(e.target).attr("id") === "rules") {
+        if ($(e.target).attr("id") === "rules") {
             $("#rulesBox").css("display", "block");
             $("#navBox").off("click");
            
-        } else if($(e.target).attr("id") === "story") {
+        } else if ($(e.target).attr("id") === "story") {
             $("#storyBox").css("display", "block");
             $("#navBox").off("click");
 
-        } else if($(e.target).attr("id") === "play") {
+        } else if ($(e.target).attr("id") === "play") {
             $(".disc").css("display", "block");
             play();
-        //                                                                      test();
+        
         };
-
-        //                                                                    $("#navBox").click(function(e) {
-        //                                                                            let modalClass = 
-        //                                                                            $(".modal").css("display", "block");  $(`$(e.target).modal $(`.modal$(e.target`) 
-        //                                                                        });
-        //                                                                                                 let targetModal = `$(.modal$(e.target))`
-
-
     };
 
 
 //---------------------  function to close pop-ups once clicked:  --------------------------
+
 
     $(".close").click(() => {
         $(".modal").css("display", "none");
@@ -63,29 +45,24 @@ $(() => {
 
     
     function play() {
-        //console.log(selectedDisc);
-
+        
         $("#stacks .discDiv").on("click", firstClick)
         
         function firstClick(e) {
             console.log(e.target);
             console.log(e.delegateTarget);
             let $clickedDiv = e.delegateTarget;
-            //console.log($clickedDiv);
-            // console.log($(e.target).find(":first-child"));
-            // $(e.target).find(":first-child").css("background-color", "purple");
-            // console.log($clickedDiv);
-            // selectedDisc = [$(e.target.firstchild)]
-            // $(selectedDisc).css("background-color", "purple");
             // console.log($clickedDiv);
             // console.log(selectedDisc)
+
+
+    //---------------------  Conditionals to select and move discs:  --------------------------
+
 
             if (selectedDisc.length === 0) {
                 //if($(e.target).classlist.contains(discDiv)) {
                     $discToAppend = $(e.delegateTarget).find(":first-child");
                     $discToAppend.addClass("selected");
-                    //$discToAppend.css("color", "red");
-                    //$discToAppend.css("background-color", "purple");
 
                 //} else {
                     //$discToAppend = $(e.target)
@@ -93,116 +70,117 @@ $(() => {
                     //$discToAppend.addClass("selected");
                     //console.log("this is e.target" + $discToAppend);
                 //}
-    //                                                                  $(e.target).find(":first-child").css("background-color", "purple");
+ 
                     selectedDisc.push($(e.delegateTarget).find(":first-child").data("index"));
-                    console.log(selectedDisc);
-                //console.log("this is where it should select a disc and change it's color");
+                    console.log("after step one: " + selectedDisc);
 
-            } else if(selectedDisc.length === 1) {
+//----------------  Set Conditionals to remove selected disc from current array for tracking of moves  ----------  
 
-                    $discToAppend.removeClass("selected");
-                    $discToAppend.prependTo($clickedDiv);
+                    if ($($clickedDiv).attr("id") === "discDiv1") {
+                        startDivArray.shift()
+                        console.log("this is where you remove selected disc from discDiv1's array: " + startDivArray);
+
+                    } else if ($($clickedDiv).attr("id") === "discDiv2") {
+                        stagingDivArray.shift()
+                        console.log("this is where you remove selected disc from discDiv2's array: " + stagingDivArray);
+
+                    } else if ($($clickedDiv).attr("id") === "discDiv3") {
+                        targetDivArray.shift()
+                        console.log("this is where you remove selected disc from disDiv3's array: " + targetDivArray);
+
+                    }
+                
+//----------------  Set Conditionals after disc is selected to control moving selected disc (seletedDisc.lenth === 1) ----------
+
+            } else if (selectedDisc.length === 1) {
+                console.log($($clickedDiv).attr("id"));
+                console.log($($clickedDiv).data("array"));
+               
+//                    selectedDisc = [];
+                    if ($($clickedDiv).attr("id") === "discDiv1") {
+                        console.log("you made it to right before your array comparisons")
+                        console.log(targetDivArray[0]);
+                        console.log(selectedDisc[0]);
+
+                        if (startDivArray[0] > selectedDisc[0] || startDivArray[0] == undefined) {
+                            console.log($discToAppend);
+                            $discToAppend.removeClass("selected");
+                            $discToAppend.prependTo($clickedDiv);
+                            startDivArray.push($($discToAppend).data("index"))
+                            console.log("this is where you update discDiv1's array: " + startDivArray);
+
+                        } else {
+                            $discToAppend.removeClass("selected");
+                            selectedDisc = [];
+                        }
+                        
+                    } else if ($($clickedDiv).attr("id") === "discDiv2") {
+                        if (stagingDivArray[0] > selectedDisc[0] || stagingDivArray[0] == undefined) {
+                            $discToAppend.removeClass("selected");
+                            $discToAppend.prependTo($clickedDiv);
+                            stagingDivArray.push($($discToAppend).data("index"))
+                            console.log("this is where you update discDiv2's array: " + stagingDivArray);
+                        } else {
+                            $discToAppend.removeClass("selected");
+                            selectedDisc = [];
+                        }
+
+                    } else if ($($clickedDiv).attr("id") === "discDiv3") {
+                        console.log("you made it");
+                        console.log(targetDivArray[0]);
+                        console.log(selectedDisc[0]);
+                        if (targetDivArray[0] > selectedDisc[0] || targetDivArray[0] == undefined) {
+
+                            $discToAppend.removeClass("selected");
+                            $discToAppend.prependTo($clickedDiv);
+                            targetDivArray.push($($discToAppend).data("index"))
+                            console.log("this is where you update disDiv3's array: " + targetDivArray);
+
+                        } else {
+                            $discToAppend.removeClass("selected");
+                            selectedDisc = [];
+                        }
+
+                    } 
+                
+                // } else { function illegalMoveAlert() {
+                //     preventDefault()
+                //     alert("You cannot put a larger disc on top of a smaller disc.  Please try again!");
+                // }}
                     selectedDisc = [];
-                    if($($clickedDiv).attr("id") === "discDiv2") {
-                        console.log("this is where you update disDiv2's array");
-                        stagingDivArray.push($($discToAppend).data("index"))
-                        console.log(stagingDivArray);
 
-
-                    } else if($($clickedDiv).attr("id") === "discDiv3") {
-                        console.log("this is where you update disDiv3's array");
-
-                    } else if($($clickedDiv).attr("id") === "discDiv1") {
-                        console.log("this is where you update disDiv1's array");
-                    };
-                    //if($clickedDiv === )
-
-            //    console.log($("#discDiv1"));
-            //    $clickedDiv.prepend($("#disc1"))
-            //     e.target.append()
             
-            } else if  (targetDivArray.length === winningCompareArray.length){
+            } //else if (targetDivArray.length === winningCompareArray.length){
             //                                                  alert("You Won!!!  Great JOB!")
-            console.log(selectedDisc[0]);
-            }
+            //console.log(selectedDisc[0]);
+            //}
             
 
            
         };
     
 
-    //     if (selectedDisc[0] === undefined) {
 
-    //         $("#discDiv1").click($selectDisc)
-    //             $selectDisc(e);
-            
-    // //         function $selectDisc (e) {
-    // // //            console.log(e.target);
-    // //             $(e.target).css("background-color", "purple");
-    // //             selectedDisc.push($(e.target).data("index"));
-    // //             console.log(selectedDisc);
-    // //             console.log(selectedDisc[0]);
-    // //         };
-    //     } else {
-    //         alert("you already clicked");
-    //     };
-    //                                                          //     $(e.target).css("background-color", "purple");
-    //                                                              //     selectedDisc.push($(e.target).data("index"));
-    //                                                              //     console.log(selectedDisc);
-
-    //                                                              // };
-        
-
-
-
-        // $("#discDiv1").click($selectDisc())
-        // ;
-        // $("#discDiv1").on("click", function(e) {
-        //     if ($selectedDisc === null) {
-        //         $(e.target).css("background-color", "purple");
-        //     };
-        // });
 
     };
 
+    // function checkIfLegal() {
 
-    // $("#stacks").on("click", ".disc", function(e) {
-    // //                                 console.log($target);
-    // //                                 let $target = $("e.target")
-    // //                                 $("e.target").stopImmediatePropagation();
-    // //                                 e.target.style.backgroundColor = "purple";
-    //      $(e.target).css("background-color", "purple");
-    // });
+    //     if ($($clickedDiv).attr("id") === "discDiv1") {
+    //         if (startDivArray[0] > selectedDisc);
 
-    // $("#stacks").click(function() {
-        
-        
-    // })
+    //     } else if ($($clickedDiv).attr("id") === "discDiv2") {
+    //         stagingDivArray.push($($discToAppend).data("index"))
+    //         console.log("this is where you update discDiv2's array: " + stagingDivArray);
 
-    //     function $selectDisc(e) {
-    //                 $selectedDisc = $(e.target);
-    //                 $(e.target).css("background-color", "purple");
-    //             }
-                
-    //     function $moveDisc(e) {
-    //         $(e.target).append($selectedDisc);
-    //     }
-    // //                                  $("#disc1").css("background-color", "purple");
+    //     } else if ($($clickedDiv).attr("id") === "discDiv3") {
+    //         targetDivArray.push($($discToAppend).data("index"))
+    //         console.log("this is where you update disDiv3's array: " + targetDivArray);
 
+    //     } 
+    // }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  
 
 
 
--- Sandbox/app2.js
+++ Sandbox/app2.js
@@ -10,11 +10,149 @@ if($(e.target).attr("id") === "rules") {
 //                                                                  test();
 };
 
-// $("#navBox").click(function(e) {
-    //     let modalClass = 
-    //     $(".modal").css("display", "block");  $(`$(e.target).modal $(`.modal$(e.target`) 
-    // });
-    //                          let targetModal = `$(.modal$(e.target))`
 
 
-});
+function selectDisc() {
+    change color of div to purple
+
+}
+
+function moveDisc() {
+    discDivToAppendTo.prepend(discDivtomove)
+    update discDiv arrays
+}
+
+function checkMove() {
+    
+    if (discDivToAppendTo < selectedDisc[0].data("id")) {
+        execute move 
+
+    } else {
+        dont execute move
+    }
+
+}
+
+
+
+//-------------------------------    before changing: 11/15/21   -------------------------------------->
+
+function firstClick(e) {
+    console.log(e.target);
+    console.log(e.delegateTarget);
+    //let clickedDiv = e.target;
+    //console.log(clickedDiv);
+    // console.log($(e.target).find(":first-child"));
+    // $(e.target).find(":first-child").css("background-color", "purple");
+    // console.log(clickedDiv);
+    // selectedDisc = [$(e.target.firstchild)]
+    // $(selectedDisc).css("background-color", "purple");
+    // console.log(clickedDiv);
+    // console.log(selectedDisc)
+
+    if (selectedDisc.length === 0) {
+        if($(e.target).classlist.contains(discDiv)) {
+            $discToAppend = $(e.target).find(":first-child");
+            $discToAppend.addClass("selected");
+            //$discToAppend.css("background-color", "purple");
+
+        } else {
+            $discToAppend = $(e.target)
+            $discToAppend.css("background-color", "purple");
+            $discToAppend.addClass("selected");
+            console.log("this is e.target" + $discToAppend);
+        }
+//                                                                  $(e.target).find(":first-child").css("background-color", "purple");
+            selectedDisc.push($(e.target).find(":first-child").data("index"));
+            console.log(selectedDisc);
+        //console.log("this is where it should select a disc and change it's color");
+
+    } else if(selectedDisc.length === 1) {
+            console.log("this is where it will be appended");
+            $discToAppend.prependTo(clickedDiv);
+            selectedDisc = [];
+    //    console.log($("#discDiv1"));
+    //    clickedDiv.prepend($("#disc1"))
+    //     e.target.append()
+    
+    } else if  (targetDivArray.length === winningCompareArray.length){
+    //                                                  alert("You Won!!!  Great JOB!")
+    console.log(selectedDisc[0]);
+    }
+    
+
+   
+};
+
+//-------------------------------    f() Play() Before Changing/Clean Up: 11/16/21   -------------------------------------->
+
+function play() {
+        
+    $("#stacks .discDiv").on("click", firstClick)
+    
+    function firstClick(e) {
+        console.log(e.target);
+        console.log(e.delegateTarget);
+        let $clickedDiv = e.delegateTarget;
+        // console.log($clickedDiv);
+        // console.log($(e.target).find(":first-child"));
+        // console.log(selectedDisc)
+
+
+//---------------------  Conditionals to select and move discs:  --------------------------
+
+
+        if (selectedDisc.length === 0) {
+            //if($(e.target).classlist.contains(discDiv)) {
+                $discToAppend = $(e.delegateTarget).find(":first-child");
+                $discToAppend.addClass("selected");
+
+            //} else {
+                //$discToAppend = $(e.target)
+                //$discToAppend.css("background-color", "purple");
+                //$discToAppend.addClass("selected");
+                //console.log("this is e.target" + $discToAppend);
+            //}
+//                                                                  $(e.target).find(":first-child").css("background-color", "purple");
+                selectedDisc.push($(e.delegateTarget).find(":first-child").data("index"));
+//                    console.log(selectedDisc);
+            
+
+        } else if(selectedDisc.length === 1) {
+
+                $discToAppend.removeClass("selected");
+                $discToAppend.prependTo($clickedDiv);
+//                    selectedDisc = [];
+
+                if($($clickedDiv).attr("id") === "discDiv2") {
+                    stagingDivArray.push($($discToAppend).data("index"))
+
+                    console.log("this is where you update discDiv2's array: " + stagingDivArray);
+
+
+                } else if($($clickedDiv).attr("id") === "discDiv3") {
+                    targetDivArray.push($($discToAppend).data("index"))
+                    console.log("this is where you update disDiv3's array: " + targetDivArray);
+
+                } else if($($clickedDiv).attr("id") === "discDiv1") {
+                    startDivArray.push($($discToAppend).data("index"))
+                    console.log("this is where you update disDiv1's array: " + startDivArray);
+                }
+
+                selectedDisc = [];
+
+        
+        } else if  (targetDivArray.length === winningCompareArray.length){
+        //                                                  alert("You Won!!!  Great JOB!")
+        console.log(selectedDisc[0]);
+        }
+        
+
+       
+    };
+
+
+
+
+};
+
